b078bc707e275adc8cd4a757e34988746cc43bcb
Dariia-Smyrnova <dariia.smyrnova@wesoftyou.com>
2023-01-15 16:37:53 -0600

    menu etc

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,9 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Markup, Telegraf } from "telegraf";
-import { PrismaClient } from '@prisma/client'
+import { Markup, Telegraf, Telegram } from "telegraf";
+import { PrismaClient, Expense } from '@prisma/client'
 
 const bot = new Telegraf(process.env.NEXT_TELEGRAM_TOKEN as string);
 const prisma = new PrismaClient()
+const telegram = new Telegram(process.env.NEXT_TELEGRAM_TOKEN);
+
+telegram.setMyCommands(
+  [
+    {"command":"spent", "description": "Add new expense"},
+    {"command":"list", "description": "list your spendings"},
+    {"command":"all", "description": "Start bot"}, 
+    {"command":"month", "description": "Show expenses for the last month"}, 
+    {"command":"year", "description": "Show expenses for the last year"},
+  ]);
 
 /**
  * hears method is used to listen to a specific text
@@ -35,7 +45,7 @@ bot.command("start", async (ctx) => {
   const user = await prisma.user.create({
     data: {
       username: user_data.first_name,
-      tg_id: user_data.id,
+      id: user_data.id,
       is_bot: user_data.is_bot,
       first_name: user_data.first_name,
       language_code: user_data.language_code,
@@ -44,32 +54,147 @@ bot.command("start", async (ctx) => {
   console.log(user);
 });
 
-bot.action("yes", async (ctx) => {
-  await ctx.answerCbQuery("You said No");
-});
+bot.command("spent", async (ctx) => {
+  const expense_str = ctx.update.message.text;
+  let splitted = expense_str.split(" ", 10);
+  let amount = parseFloat(splitted[1]);
+  let currency = splitted[2];
+  let category = splitted[3];
 
-bot.action("no", async (ctx) => {
-  await ctx.answerCbQuery("You said No");
-});
+  let expense = await prisma.expense.create({
+    data: {
+      userId: ctx.message.from.id,
+      amount: amount,
+      currency: currency,
+      category: category,
+    },
+  }
+  )
+})
 
-// This how you set up expcility commands
-bot.command("caption", (ctx) => {
-  return ctx.replyWithPhoto(
-    { url: "https://picsum.photos/200/300/?random" },
-    {
-      caption: "Caption",
-      parse_mode: "Markdown",
-      ...Markup.inlineKeyboard([
-        Markup.button.callback("Plain", "plain"),
-        Markup.button.callback("Italic", "italic"),
-      ]),
-    }
-  );
-});
+bot.command("list", async (ctx) => {
+  const all_expenses = await prisma.expense.findMany({
+    where: {
+      userId: {
+        equals: ctx.message.from.id,
+      },
+    },
+    orderBy: {
+      added: 'desc',
+    },
+  })
+  ctx.replyWithMarkdownV2(`
+  \\#\\# Money
+  ${all_expenses.map((e: Expense) => `${e.added.toDateString()} ${e.category} ${e.amount} ${e.currency?.toUpperCase()}`).join("\n")}`)
+})
 
-bot.action(/.+/, (ctx) => {
-  return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`);
-});
+bot.command("all", async (ctx) => {
+  const byCategory = await prisma.expense.groupBy({
+    by: ['category'],
+    where: {
+      userId: {
+        equals: ctx.message.from.id,
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  })
+  
+  console.log(JSON.stringify(byCategory))
+  let str = ['*Spent by category*']
+  for (const cat of byCategory) {
+    str.push(`*${cat.category}:* ${cat._sum.amount}`)
+  }
+
+  ctx.replyWithMarkdownV2(str.join('\n'))
+})
+
+bot.command("month", async (ctx) => {
+let monthAgo = new Date();
+monthAgo.setMonth(monthAgo.getMonth() - 1);
+monthAgo.setHours(0, 0, 0, 0);
+
+  const byCategory = await prisma.expense.groupBy({
+    by: ['category'],
+    where: {
+      userId: {
+        equals: ctx.message.from.id,
+      },
+      added: {
+        gt: monthAgo,
+      }
+    },
+    _sum: {
+      amount: true,
+    },
+  })
+  
+  console.log(JSON.stringify(byCategory))
+  let str = ['*Spent by category*']
+  for (const cat of byCategory) {
+    str.push(`*${cat.category}:* ${cat._sum.amount}`)
+  }
+
+  ctx.replyWithMarkdownV2(str.join('\n'))
+})
+
+bot.command("year", async (ctx) => {
+let yearAgo = new Date();
+yearAgo.setFullYear(yearAgo.getFullYear() - 1);
+yearAgo.setHours(0, 0, 0, 0);
+
+  const byCategory = await prisma.expense.groupBy({
+    by: ['category'],
+    where: {
+      userId: {
+        equals: ctx.message.from.id,
+      },
+      added: {
+        gt: yearAgo,
+      }
+    },
+    _sum: {
+      amount: true,
+    },
+  })
+  
+  console.log(JSON.stringify(byCategory))
+  let str = ['*Spent by category*']
+  for (const cat of byCategory) {
+    str.push(`*${cat.category}:* ${cat._sum.amount}`)
+  }
+
+  ctx.replyWithMarkdownV2(str.join('\n'))
+})
+
+
+// const PERIOD_BUTTONS = Markup.keyboard([["/month", "/30_days", "/all"]])
+// bot.command("period", async (ctx) => {
+//   ctx.reply(
+//     "Select a period for which you want to see expenses",
+//     PERIOD_BUTTONS.resize()
+//   );
+// })
+
+// // This how you set up expcility commands
+// bot.command("caption", (ctx) => {
+//   return ctx.replyWithPhoto(
+//     { url: "https://picsum.photos/200/300/?random" },
+//     {
+//       caption: "Caption",
+//       parse_mode: "Markdown",
+//       ...Markup.inlineKeyboard([
+//         Markup.button.callback("Plain", "plain"),
+//         Markup.button.callback("Italic", "italic"),
+//       ]),
+//     }
+//   );
+// });
+
+// bot.action(/.+/, (ctx) => {
+//   return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`);
+// });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await bot.handleUpdate(req.body, res);
